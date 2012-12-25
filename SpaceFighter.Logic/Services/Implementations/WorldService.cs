f023cd3f9126258bbb476b735abba82cdb30883a
Thomas Kudlacek <thomas_kudlacek@hotmail.com>
2012-12-25 22:47:15 +0100

    Dynamic tile loading with help of ContentManifestExtensions.dll.

--- SpaceFighter.Logic/Services/Implementations/WorldService.cs
+++ SpaceFighter.Logic/Services/Implementations/WorldService.cs
@@ -5,6 +5,9 @@
 namespace SpaceFighter.Logic.Services.Implementations
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     using Microsoft.Xna.Framework;
     using Microsoft.Xna.Framework.Graphics;
 
@@ -18,7 +21,7 @@ namespace SpaceFighter.Logic.Services.Implementations
         private readonly int verticalTileCount;
         private SpriteBatch spriteBatch;
 
-        private Texture2D sprite;
+        private readonly List<Texture2D> spriteList = new List<Texture2D>();
 
         public WorldService(Game game) : base(game)
         {
@@ -27,15 +30,19 @@ namespace SpaceFighter.Logic.Services.Implementations
 
             horizontalTileCount = screenWidth / TileSize;
             verticalTileCount = screenHeight / TileSize;
-            this.tileMap = new int[horizontalTileCount, verticalTileCount];
 
-            for (int i = 0; i < horizontalTileCount; i++)
-            {
-                for (int j = 0; j < verticalTileCount; j++)
+            this.tileMap = new[,]
                 {
-                    this.tileMap[i, j] = 0x01;
-                }
-            }
+                    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x02, 0x03, 0x00, 0x00, 0x00, 0x00 },
+                    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x05, 0x05, 0x06, 0x00, 0x00, 0x00, 0x00 },
+                    { 0x00, 0x00, 0x01, 0x02, 0x03, 0x00, 0x00, 0x00, 0x00, 0x07, 0x08, 0x09, 0x00, 0x00, 0x00, 0x00 },
+                    { 0x00, 0x00, 0x04, 0x05, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
+                    { 0x00, 0x00, 0x07, 0x08, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
+                    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x02, 0x03, 0x00 },
+                    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x05, 0x05, 0x06, 0x00 },
+                    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x08, 0x08, 0x09, 0x00 },
+                    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+                };
         }
 
         public void LoadWorld()
@@ -51,7 +58,13 @@ namespace SpaceFighter.Logic.Services.Implementations
         protected override void LoadContent()
         {
             this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
-            this.sprite = this.Game.Content.Load<Texture2D>("Sprites/L1/L1_01");
+
+            var tileList = this.Game.Content.Load<List<string>>("manifest").Where(x => x.StartsWith(@"Sprites\L1")).ToList();
+
+            foreach (var tile in tileList)
+            {
+                spriteList.Add(this.Game.Content.Load<Texture2D>(tile));
+            }
 
             base.LoadContent();
         }
@@ -65,11 +78,11 @@ namespace SpaceFighter.Logic.Services.Implementations
         {
             this.spriteBatch.Begin();
 
-            for (int i = 0; i < horizontalTileCount; i++)
+            for (int i = 0; i < verticalTileCount; i++)
             {
-                for (int j = 0; j < verticalTileCount; j++)
+                for (int j = 0; j < horizontalTileCount; j++)
                 {
-                    this.spriteBatch.Draw(this.sprite, new Vector2(i * TileSize, j * TileSize), Color.White);
+                    this.spriteBatch.Draw(this.spriteList[this.tileMap[i,j]], new Vector2(j * TileSize, i * TileSize), Color.White);
                 }
             }
 
