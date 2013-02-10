ccd0f30b88ef29cf5943bbea27083b86dc0add8f
Thomas Kudlacek <thomas_kudlacek@hotmail.com>
2013-02-10 17:41:48 +0100

    Pulled specific stuff out of EnemyBase to EnemyGreen.

--- SpaceFighter.Logic/Entities/Implementations/Enemies/EnemyBase.cs
+++ SpaceFighter.Logic/Entities/Implementations/Enemies/EnemyBase.cs
@@ -7,102 +7,39 @@ namespace SpaceFighter.Logic.Entities.Implementations.Enemies
     using System;
     using Microsoft.Xna.Framework;
     using Microsoft.Xna.Framework.Graphics;
-
     using SpaceFighter.Logic.AI;
     using SpaceFighter.Logic.Entities.Interfaces;
     using SpaceFighter.Logic.Services.Interfaces;
     using SpaceFighter.Logic.StateMachine;
 
-    public class EnemyBase : DrawableGameComponent, IEnemy
+    public abstract class EnemyBase : DrawableGameComponent, IEnemy
     {
-        private Vector2 position;
         private SpriteBatch spriteBatch;
-        private Color[] colorData;
-        private int health = 100;       
-        private float rotation;
-        private double angleToPlayer;
 
         private ICameraService cameraService;
-        private StateMachine<Action<double>> stateMachine;
-        private SpriteManager spriteManager;
-        private bool isAlive;
         private Vector2 distanceToPlayer;
         private ISteering steeringStrategy;
 
-        public EnemyBase(Game game, Vector2 startPosition) : base(game)
+        protected StateMachine<Action<double>> stateMachine;
+        protected SpriteManager spriteManager;
+
+        protected EnemyBase(Game game, Vector2 startPosition) : base(game)
         {
-            this.isAlive = true;
-            this.position = startPosition;
+            this.Position = startPosition;
             this.steeringStrategy = new SteeringSeek();
             
             this.Game.Components.Add(this);
         }
 
-        public int Health
-        {
-            get
-            {
-                return this.health;
-            }
-        }
+        public abstract void InitializeStateMachine();
+        public abstract void LoadSprites();
+        public abstract bool IsAlive { get; }
 
-        public Rectangle BoundingRectangle
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public bool IsAlive
-        {
-            get
-            {
-                return this.isAlive;
-            }
-        }
-
-        public void SubtractHealth(int amount)
-        {
-            this.health -= amount;
-        }
-
-        public void AddHealth(int amount)
-        {
-            this.health += amount;
-        }
-
-        public float Rotation
-        {
-            get
-            {
-                return this.rotation;
-            }
-        }
-
-        public Vector2 Position
-        {
-            get
-            {
-                return this.position;
-            }
-        }
-
-        public Vector2 Origin
-        {
-            get
-            {
-                return new Vector2(this.position.X + (this.Width / 2.0f), this.position.Y + (this.Height / 2.0f));
-            }
-        }
-
-        public Color[] ColorData
-        {
-            get
-            {
-                return this.colorData;
-            }
-        }
+        public int Health { get; protected set; }
+        public float Rotation { get; private set; }
+        public Vector2 Position { get; private set; }
+        public double AngleToPlayer { get; private set; }
+        public Color[] ColorData { get; private set; }
 
         public int Width
         {
@@ -120,17 +57,35 @@ namespace SpaceFighter.Logic.Entities.Implementations.Enemies
             }
         }
 
-        public double AngleToPlayer
+        public Vector2 Origin
         {
             get
             {
-                return this.angleToPlayer;
+                return new Vector2(this.Position.X + (this.Width / 2.0f), this.Position.Y + (this.Height / 2.0f));
             }
+        }
+
+        public Rectangle BoundingRectangle
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void SubtractHealth(int amount)
+        {
+            this.Health -= amount;
+        }
+
+        public void AddHealth(int amount)
+        {
+            this.Health += amount;
         }
 
         public void UpdateAngleToPlayer(double angle)
         {
-            this.angleToPlayer = angle;
+            this.AngleToPlayer = angle;
         }
 
         public void UpdateDistanceToPlayer(Vector2 distance)
@@ -145,32 +100,6 @@ namespace SpaceFighter.Logic.Entities.Implementations.Enemies
             base.Initialize();
         }
 
-        private void InitializeStateMachine()
-        {
-            var alive = new State<Action<double>>(
-                EnemyState.Alive,
-                null,
-                null,
-                null);
-
-            var dying = new State<Action<double>>(
-                EnemyState.Dying,
-                null,
-                null,
-                null);
-
-            var dead = new State<Action<double>>(
-                EnemyState.Dead,
-                null,
-                () => this.isAlive = false,
-                null);
-
-            alive.AddTransition(dying, () => this.Health <= 0);
-            dying.AddTransition(dead, () => this.spriteManager.IsAnimationDone(this.stateMachine.CurrentState.Name));
-
-            this.stateMachine = new StateMachine<Action<double>>(alive);
-        }
-
         /// <summary>
         /// Called when graphics resources need to be loaded. Override this method to load any component-specific graphics resources.
         /// </summary>
@@ -178,20 +107,7 @@ namespace SpaceFighter.Logic.Entities.Implementations.Enemies
         {
             this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
 
-            this.spriteManager = new SpriteManager(EnemyState.Alive, 108, 128);
-
-            this.spriteManager.AddStillSprite(
-                EnemyState.Alive,
-                this.Game.Content.Load<Texture2D>("Sprites/Enemy/Alive"));
-
-            this.spriteManager.AddAnimatedSprite(
-                EnemyState.Dying,
-                this.Game.Content.Load<Texture2D>("Sprites/Enemy/Dying"));
-
-            this.spriteManager.AddStillSprite(
-                EnemyState.Dead,
-                this.Game.Content.Load<Texture2D>("Sprites/Enemy/Dead"));
-
+            this.LoadSprites();
             this.UpdateSpriteColorData();
 
             base.LoadContent();
@@ -214,10 +130,10 @@ namespace SpaceFighter.Logic.Entities.Implementations.Enemies
 
             this.spriteBatch.Draw(
                 this.spriteManager.GetCurrentSprite(),
-                this.position,
+                this.Position,
                 this.spriteManager.GetCurrentRectangle(),
                 Color.White,
-                this.rotation,
+                this.Rotation,
                 new Vector2(this.Width / 2.0f, this.Height / 2.0f),
                 1.0f, 
                 SpriteEffects.None, 
@@ -233,18 +149,18 @@ namespace SpaceFighter.Logic.Entities.Implementations.Enemies
             this.stateMachine.Update();
             this.spriteManager.Update(this.stateMachine.CurrentState.Name, gameTime);
            
-            var previousPositionX = this.position.X;
-            var previousPositionY = this.position.Y;
+            var previousPositionX = this.Position.X;
+            var previousPositionY = this.Position.Y;
 
-            this.position = this.steeringStrategy.AdvancePosition(this.position, this.distanceToPlayer, this.rotation);
-            this.rotation = (float)Math.Atan2(this.position.Y - previousPositionY, this.position.X - previousPositionX);
+            this.Position = this.steeringStrategy.AdvancePosition(this.Position, this.distanceToPlayer, this.Rotation);
+            this.Rotation = (float)Math.Atan2(this.Position.Y - previousPositionY, this.Position.X - previousPositionX);
 
             base.Update(gameTime);
         }
 
         private void UpdateSpriteColorData()
         {
-            this.colorData = new Color[this.spriteManager.GetCurrentSprite().Width * this.spriteManager.GetCurrentSprite().Height];
+            this.ColorData = new Color[this.spriteManager.GetCurrentSprite().Width * this.spriteManager.GetCurrentSprite().Height];
             this.spriteManager.GetCurrentSprite().GetData(this.ColorData);
         }
     }
--- SpaceFighter.Logic/Entities/Implementations/Enemies/EnemyGreen.cs
+++ SpaceFighter.Logic/Entities/Implementations/Enemies/EnemyGreen.cs
@@ -3,13 +3,74 @@
 // -----------------------------------------------------------------------
 
 namespace SpaceFighter.Logic.Entities.Implementations.Enemies
-{   
+{
+    using System;
     using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    using SpaceFighter.Logic.StateMachine;
 
     public class EnemyGreen : EnemyBase
     {
+        private bool isAlive;
+
         public EnemyGreen(Game game, Vector2 startPosition) : base(game, startPosition)
         {
+            this.isAlive = true;
+            this.Health = 100;
+        }
+
+        public override bool IsAlive
+        {
+            get
+            {
+                return this.isAlive;
+            }
+        }
+
+        public override void InitializeStateMachine()
+        {
+            var alive = new State<Action<double>>(
+                EnemyState.Alive,
+                null,
+                null,
+                null);
+
+            var dying = new State<Action<double>>(
+                EnemyState.Dying,
+                null,
+                null,
+                null);
+
+            var dead = new State<Action<double>>(
+                EnemyState.Dead,
+                null,
+                () => this.isAlive = false,
+                null);
+
+            alive.AddTransition(dying, () => this.Health <= 0);
+            dying.AddTransition(dead, () => this.spriteManager.IsAnimationDone(this.stateMachine.CurrentState.Name));
+
+            this.stateMachine = new StateMachine<Action<double>>(alive);
+        }
+
+        public override void LoadSprites()
+        {
+            this.spriteManager = new SpriteManager(EnemyState.Alive, 108, 128);
+
+            this.spriteManager.AddStillSprite(
+                EnemyState.Alive,
+                this.Game.Content.Load<Texture2D>("Sprites/Enemy/Alive"));
+
+            this.spriteManager.AddAnimatedSprite(
+                EnemyState.Dying,
+                this.Game.Content.Load<Texture2D>("Sprites/Enemy/Dying"));
+
+            this.spriteManager.AddStillSprite(
+                EnemyState.Dead,
+                this.Game.Content.Load<Texture2D>("Sprites/Enemy/Dead"));
         }
     }
 }
+
+
