fc66da42b55dd0762fb3e94215ca369229afbbca
Thomas Kudlacek <thomas_kudlacek@hotmail.com>
2013-03-15 22:29:36 +0100

    2nd enemy.

--- SpaceFighter.Logic/Services/Implementations/EnemyService.cs
+++ SpaceFighter.Logic/Services/Implementations/EnemyService.cs
@@ -38,6 +38,7 @@ namespace SpaceFighter.Logic.Services.Implementations
         public override void Initialize()
         {
             EnemyFactory.Create<EnemyGreen>(this.Game, new Vector2(400, 400));
+            EnemyFactory.Create<EnemyGreen>(this.Game, new Vector2(600, 400));
             base.Initialize();
         }
 
