1343f6994a478714504dbea79c8f82774f87392f
Thomas Kudlacek <thomas_kudlacek@hotmail.com>
2013-03-24 21:00:39 +0100

    Wander steering strategy (implementation outstanding)

---
+++ SpaceFighter.Logic/Entities/Implementations/SteeringStrategies/SteeringStrategyWander.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// (c) Cataclysm Game Studios 2012
+// -----------------------------------------------------------------------
+
+namespace SpaceFighter.Logic.Entities.Implementations.SteeringStrategies
+{
+    using System;
+    using Microsoft.Xna.Framework;
+    using SpaceFighter.Logic.Entities.Interfaces;
+
+    public class SteeringStrategyWander : ISteeringStrategy
+    {
+        public Vector2 Execute(Vector2 position, Vector2 distance, float angle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
