866235b8bf4bd936b6ecf6c4098a06f33b277c6b
Thomas Kudlacek <thomas_kudlacek@hotmail.com>
2013-04-04 22:02:35 +0200

    Wired up enemy states "patrol", "attack" and "retreat" in respective state engine (with dummy wandering steering behavior).

--- SpaceFighter.Logic/EnemyState.cs
+++ SpaceFighter.Logic/EnemyState.cs
@@ -7,6 +7,9 @@ namespace SpaceFighter.Logic
     public static class EnemyState
     {
         public const string Alive = "Alive";
+        public const string Patrol = "Patrol";
+        public const string Attack = "Attack";
+        public const string Retreat = "Retreat";
         public const string Dying = "Dying";
         public const string Dead = "Dead";
     }
