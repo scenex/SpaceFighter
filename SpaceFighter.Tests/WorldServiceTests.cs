bd2a4a143e3b83bc428f78463d92f2953c3b6c3c
Thomas Kudlacek <thomas_kudlacek@hotmail.com>
2013-05-05 15:08:08 +0200

    Added initial WorldService tests file.

---
+++ SpaceFighter.Tests/WorldServiceTests.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// (c) Cataclysm Game Studios 2012
+// -----------------------------------------------------------------------
+
+namespace SpaceFighter.Tests
+{
+    using SpaceFighter.Logic.Services.Implementations;
+    using SpaceFighter.Logic.Services.Interfaces;
+
+    using Xunit;
+
+    public class WorldServiceTests
+    {
+        private readonly IWorldService testee;
+
+        public WorldServiceTests()
+        {
+            this.testee = new WorldService(null);
+        }
+    }
+}
