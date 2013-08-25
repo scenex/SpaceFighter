b1108a525cde8ec3d519f4b4b8991f90267fe652
Thomas Kudlacek <thomas_kudlacek@hotmail.com>
2013-08-25 11:31:35 +0200

    Menu fade in & fade out.

--- SpaceFighter.Logic/Screens/MenuScreen.cs
+++ SpaceFighter.Logic/Screens/MenuScreen.cs
@@ -17,6 +17,13 @@ namespace SpaceFighter.Logic.Screens
 
         private SpriteBatch spriteBatch;
         private SpriteFont spriteFont;
+
+        private Curve fadeInCurve;
+        private Curve fadeOutCurve;
+        private Curve currentFadeCurve;
+
+        private const double TransitionTime = 1500;
+        private double elapsedTime;
         private int selectionIndex;
 
         public MenuScreen(Game game, IInputService inputService) : base(game)
@@ -30,30 +37,43 @@ namespace SpaceFighter.Logic.Screens
         protected override void LoadContent()
         {
             this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
-            this.spriteFont = this.Game.Content.Load<SpriteFont>(@"FramerateFont"); 
+            this.spriteFont = this.Game.Content.Load<SpriteFont>(@"FramerateFont");
+
+            this.fadeInCurve = this.Game.Content.Load<Curve>(@"Curves\MenuTextFadeIn");
+            this.fadeOutCurve = this.Game.Content.Load<Curve>(@"Curves\MenuTextFadeOut");
+            this.currentFadeCurve = fadeInCurve;
 
             base.LoadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
+            this.elapsedTime += gameTime.ElapsedGameTime.TotalMilliseconds;
+
             if (this.inputService.IsSelectionConfirmed)
             {
                 this.inputService.IsSelectionConfirmed = false;
 
                 if (this.GetSelectionIndex() == 0)
                 {
-                    //this.MenuItemSelected(this, new MenuItemSelectedEventArgs(MenuItems.StartGame));
-                    this.IsTransitionAllowed = true;
+                    this.elapsedTime = 0;
+                    this.currentFadeCurve = fadeOutCurve;
                     this.TransitionTag = MenuItems.StartGame;
+                    this.inputService.Disable();
                 }
 
                 if (this.GetSelectionIndex() == 1)
                 {
-                    //this.MenuItemSelected(this, new MenuItemSelectedEventArgs(MenuItems.ExitGame));
-                    this.IsTransitionAllowed = true;
+                    this.elapsedTime = 0;
+                    this.currentFadeCurve = fadeOutCurve;
                     this.TransitionTag = MenuItems.ExitGame;
+                    this.inputService.Disable();
                 }
+            }
+
+            if (this.TransitionTag != null && elapsedTime > TransitionTime)
+            {
+                this.IsTransitionAllowed = true;
             }
 
             if (this.inputService.IsSelectionMoveDown)
@@ -74,8 +94,23 @@ namespace SpaceFighter.Logic.Screens
         public override void Draw(GameTime gameTime)
         {
             this.spriteBatch.Begin();
-            this.spriteBatch.DrawString(this.spriteFont, "Start Game", new Vector2(570, 500), this.GetSelectionIndex() == 0 ? Color.Red : Color.White);
-            this.spriteBatch.DrawString(this.spriteFont, "Exit Game", new Vector2(570, 550), this.GetSelectionIndex() == 1 ? Color.Red : Color.White);
+
+            this.spriteBatch.DrawString(
+                this.spriteFont, 
+                "Start Game", 
+                new Vector2(570, 500), 
+                this.GetSelectionIndex() == 0
+                ? Color.Red * this.currentFadeCurve.Evaluate((float)this.elapsedTime / 1000)
+                : Color.White * this.currentFadeCurve.Evaluate((float)this.elapsedTime / 1000));
+
+            this.spriteBatch.DrawString(
+                this.spriteFont, 
+                "Exit Game", 
+                new Vector2(570, 550), 
+                this.GetSelectionIndex() == 1
+                ? Color.Red * this.currentFadeCurve.Evaluate((float)this.elapsedTime / 1000)
+                : Color.White * this.currentFadeCurve.Evaluate((float)this.elapsedTime / 1000));
+            
             this.spriteBatch.End();
 
             base.Draw(gameTime);
