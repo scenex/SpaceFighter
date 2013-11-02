4eec7cbfe8d1f96c75a5dc12bbc25e767ea9f1d8
Thomas Kudlacek <thomas_kudlacek@hotmail.com>
2013-11-02 19:43:48 +0100

    Added ability to spawn enemies in a timed manner (with triggered actions)

--- SpaceFighter.Logic/Services/Implementations/EnemyService.cs
+++ SpaceFighter.Logic/Services/Implementations/EnemyService.cs
@@ -13,13 +13,17 @@ namespace SpaceFighter.Logic.Services.Implementations
 
     public class EnemyService : GameComponent, IEnemyService
     {
-        public bool IsBossEliminated { get; private set; }
         private readonly IEnemyFactory enemyFactory;
-        
+        private readonly List<TimeTriggerActionInvoker> enemiesTimeTriggerActionInvokers = new List<TimeTriggerActionInvoker>();
+
+        private double elapsedTime;
+
         public EnemyService(Game game, IEnemyFactory enemyFactory) : base(game)
         {
             this.enemyFactory = enemyFactory;
         }
+
+        public bool IsBossEliminated { get; private set; }
 
         public IEnumerable<IEnemy> Enemies
         {
@@ -40,9 +44,9 @@ namespace SpaceFighter.Logic.Services.Implementations
         public void SpawnEnemies()
         {
             this.IsBossEliminated = false;
+            //this.enemyFactory.CreateAutonomous<EnemyAutonomous>(new Vector2(400, 400), true);      
 
             var waypoints = new Queue<Vector2>();
-
             waypoints.Enqueue(new Vector2(-80,  500));
             waypoints.Enqueue(new Vector2(0  ,  500));
             waypoints.Enqueue(new Vector2(80 ,  580));
@@ -58,14 +62,41 @@ namespace SpaceFighter.Logic.Services.Implementations
             waypoints.Enqueue(new Vector2(880,  420));
             waypoints.Enqueue(new Vector2(960,  420));
             waypoints.Enqueue(new Vector2(1040, 420));
+            this.enemiesTimeTriggerActionInvokers.Add(new TimeTriggerActionInvoker(1000, () => this.enemyFactory.CreateScripted<EnemyScripted>(waypoints, false)));
 
-            //this.enemyFactory.CreateAutonomous<EnemyAutonomous>(new Vector2(400, 400), true);          
-            this.enemyFactory.CreateScripted<EnemyScripted>(waypoints, true);
+
+            var waypoints2 = new Queue<Vector2>();
+            waypoints2.Enqueue(new Vector2(-80, 500));
+            waypoints2.Enqueue(new Vector2(0, 500));
+            waypoints2.Enqueue(new Vector2(80, 580));
+            waypoints2.Enqueue(new Vector2(160, 580));
+            waypoints2.Enqueue(new Vector2(240, 580));
+            waypoints2.Enqueue(new Vector2(320, 580));
+            waypoints2.Enqueue(new Vector2(400, 580));
+            waypoints2.Enqueue(new Vector2(480, 500));
+            waypoints2.Enqueue(new Vector2(560, 500));
+            waypoints2.Enqueue(new Vector2(640, 500));
+            waypoints2.Enqueue(new Vector2(720, 420));
+            waypoints2.Enqueue(new Vector2(800, 420));
+            waypoints2.Enqueue(new Vector2(880, 420));
+            waypoints2.Enqueue(new Vector2(960, 420));
+            waypoints2.Enqueue(new Vector2(1040, 420));
+            this.enemiesTimeTriggerActionInvokers.Add(new TimeTriggerActionInvoker(4000, () => this.enemyFactory.CreateScripted<EnemyScripted>(waypoints2, true)));
+
+            this.elapsedTime = 0;
         }
 
         public void UnspawnEnemies()
         {
-            // Todo
+            this.enemiesTimeTriggerActionInvokers.Clear();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            this.elapsedTime += gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            this.EnemySpawnTrigger(gameTime);
+            base.Update(gameTime);
         }
 
         public void ReportEnemyHit(IEnemy enemy, IShot shot)
@@ -85,6 +116,19 @@ namespace SpaceFighter.Logic.Services.Implementations
             foreach (var enemy in enemies.Where(enemy => enemy.Weapon.Shots.Contains(shot)))
             {
                 enemy.Weapon.Shots.Remove(shot);
+            }
+        }
+
+        private void EnemySpawnTrigger(GameTime gameTime)
+        {
+            foreach (var enemyTimeTriggerActionInvoker in this.enemiesTimeTriggerActionInvokers.ToList())
+            {
+                if (enemyTimeTriggerActionInvoker.TriggerMilliseconds <= this.elapsedTime)
+                {
+                    this.enemiesTimeTriggerActionInvokers.Remove(enemyTimeTriggerActionInvoker);
+                    enemyTimeTriggerActionInvoker.TriggerAction();
+
+                }
             }
         }
     }
---
+++ SpaceFighter.Logic/TimeTriggerActionInvoker.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+// (c) Cataclysm Game Studios 2012
+// -----------------------------------------------------------------------
+
+namespace SpaceFighter.Logic
+{
+    using System;
+
+    public class TimeTriggerActionInvoker
+    {
+        private readonly Action triggerAction;
+        private readonly double triggerMilliseconds;
+
+        public TimeTriggerActionInvoker(double triggerMilliseconds, Action triggerAction)
+        {
+            this.triggerMilliseconds = triggerMilliseconds;
+            this.triggerAction = triggerAction;
+        }
+
+        public double TriggerMilliseconds
+        {
+            get
+            {
+                return this.triggerMilliseconds;
+            }
+        }
+
+        public void TriggerAction()
+        {
+            this.triggerAction.Invoke();
+        }
+    }
+}
