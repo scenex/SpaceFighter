2b471bbc4c07b7ca6e18c409c4748671cad40648
Thomas Kudlacek <thomas_kudlacek@hotmail.com>
2014-02-09 12:24:04 +0100

    Added SpaceFighter.Interfaces project.

---
+++ SpaceFighter.Interfaces/ISpaceFighterApi.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------
+// (c) Cataclysm Game Studios 2012
+// -----------------------------------------------------------------------
+
+namespace SpaceFighter.Interfaces
+{
+    public interface ISpaceFighterApi
+    {
+        void Load();
+        void Save();
+        void Pause();
+        void Restart();
+        void Quit();
+
+        void AddEnemy();
+        void RemoveEnemy();
+        void ListEnemeies();
+    }
+}
---
+++ SpaceFighter.Interfaces/SpaceFighterApi.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+// (c) Cataclysm Game Studios 2012
+// -----------------------------------------------------------------------
+
+namespace SpaceFighter.Interfaces
+{
+    public class SpaceFighterApi : ISpaceFighterApi
+    {
+        public void Load()
+        {
+        }
+
+        public void Save()
+        {
+        }
+
+        public void Pause()
+        {
+        }
+
+        public void Restart()
+        {
+        }
+
+        public void Quit()
+        {
+        }
+
+        public void AddEnemy()
+        {
+        }
+
+        public void RemoveEnemy()
+        {
+        }
+
+        public void ListEnemeies()
+        {
+        }
+    }
+}
