73c3d4aa6207656c80764a6dbce5116f1930c5b9
Thomas Kudlacek <thomas_kudlacek@hotmail.com>
2014-02-15 19:24:45 +0100

    Added starfield rendering to LevelService.

---
+++ SpaceFighter.Logic/Entities/Star.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// (c) Cataclysm Game Studios 2012
+// -----------------------------------------------------------------------
+
+namespace SpaceFighter.Logic.Entities
+{
+    using Microsoft.Xna.Framework;
+
+    public class Star
+    {
+        public Star(Vector2 position, int size)
+        {
+            this.Position = position;
+            this.Size = size;
+        }
+
+        public Vector2 Position {get; set;}
+        
+        public int Size { get; set; }
+
+        public int Velocity
+        {
+            get
+            {
+                if (this.Size == 1)
+                {
+                    return 3;
+                }
+
+                if (this.Size == 2)
+                {
+                    return 2;
+                }
+
+                if (this.Size == 3)
+                {
+                    return 1;
+                }
+
+                return 1;
+            }
+        }
+    }
+}
--- SpaceFighter.Logic/Services/Implementations/LevelService.cs
+++ SpaceFighter.Logic/Services/Implementations/LevelService.cs
@@ -4,12 +4,26 @@
 
 namespace SpaceFighter.Logic.Services.Implementations
 {
-    using Microsoft.Xna.Framework;
+    using System;
+    using System.Collections.Generic;
 
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    using SpaceFighter.Logic.Entities;
     using SpaceFighter.Logic.Services.Interfaces;
 
     public class TerrainService : DrawableGameComponent, ITerrainService
     {
+        private SpriteBatch spriteBatch;
+
+        private Texture2D starSmall;
+        private Texture2D starMedium;
+        private Texture2D starLarge;
+        
+        private readonly List<Star> starfield = new List<Star>();
+        private readonly Random random = new Random(5);
+
         public TerrainService(Game game) : base(game)
         {
             this.TileSize = 80;
@@ -51,6 +65,75 @@ namespace SpaceFighter.Logic.Services.Implementations
             {
                 // 9 * 80 = 720
                 return this.VerticalTileCount * TileSize;
+            }
+        }
+
+        public override void Initialize()
+        {
+            this.spriteBatch = new SpriteBatch(this.Game.GraphicsDevice);
+
+            for (var i = 0; i < 70;i++)
+            {
+                this.starfield.Add(
+                    new Star(
+                        new Vector2(random.Next(this.LevelWidth), random.Next(this.LevelHeight)),
+                        random.Next(1, 4)));
+            }
+            
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            this.starSmall = this.Game.Content.Load<Texture2D>(@"Sprites/Stars/Small");
+            this.starMedium = this.Game.Content.Load<Texture2D>(@"Sprites/Stars/Medium");
+            this.starLarge = this.Game.Content.Load<Texture2D>(@"Sprites/Stars/Large");
+            
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var star in starfield)
+            {
+                star.Position = new Vector2(star.Position.X, star.Position.Y + star.Velocity);
+
+                if (star.Position.Y > this.LevelHeight)
+                {
+                    star.Position = new Vector2(random.Next(this.LevelWidth), 0);
+                    star.Size = random.Next(1, 4);
+                }
+            }
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            this.spriteBatch.Begin();
+            
+            foreach (var star in starfield)
+            {
+                this.spriteBatch.Draw(this.MapStarSize(star.Size), star.Position, Color.White);
+            }
+
+            this.spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private Texture2D MapStarSize(int starSize)
+        {
+            switch (starSize)
+            {
+                case 1:
+                    return this.starSmall;
+                case 2:
+                    return this.starMedium;
+                case 3:
+                    return this.starLarge;
+                default:
+                    return this.starSmall;
             }
         }
 
